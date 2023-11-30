baaa0e6607892ab8e983dbc3967acf5fbf21c532
Joris Kok <joris_kok1@hotmail.com>
2023-11-30 13:35:53 +0100

    Fix docs

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
+import {Store, Fetched, Computed, Persistent} from "somestate"
 
+/**
+ * Custom hook for subscribing to changes in a store and returning the current value.
+ *
+ * @param {Store, Computed, Persistent, Fetched} store - The store object
+ * @returns {*} The current value of the store
+ */
 export const useStore = (store) => {
   const [value, setValue] = useState(store.get());
 
